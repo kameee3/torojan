3574f9c8e43e4990ee0cc4ea20ab790ed417cc6d
kameee3 <u0803096@gmail.com>
2025-08-26 07:05:13 +0900

    Update torojan.bat

--- torojan.bat
+++ torojan.bat
@@ -1,8 +1,1 @@
-@echo off
-set "TITLE=あなたのPCはウイルスに感染しました！"
-set "MSG=復元を実行する"
 
-mshta "javascript:var r=confirm('%MSG%'); if(r){alert('はいが選ばれました');} else {alert('いいえが選ばれました');}; close();"
-
-echo ^G
-pause
